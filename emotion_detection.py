a44c375bd5cafb0d68df107fc808bfbc567e0fad
Noor_Aldin <noorabdalkadir@std.mans.edu.eg>
2023-11-04 06:41:18 -0400

    task 3 done

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = input_json, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    return formatted_response
+
+def emotion_predictor(detected_text):
+    emotions = detected_text['emotionPredictions'][0]['emotion']
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    max_emotion = max(emotions, key=emotions.get)
+    #max_emotion_score = emotions[max_emotion]
+    formated_dict_emotions = {
+                            'anger': anger,
+                            'disgust': disgust,
+                            'fear': fear,
+                            'joy': joy,
+                            'sadness': sadness,
+                            'dominant_emotion': max_emotion
+                            }
+    return formated_dict_emotions
